3e0ac758001b44c0e873dfc83e91f7312492dea8
CoryXie <cory.xie@gmail.com>
2016-05-16 00:36:58 +0800

    Created StackBook (StackExchange API is based on StackJS https://launchpad.net/stackjs but adopted to be used in Atom Code Editor)

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.0.1 - First Push
+* Created StackBook (StackExchange API is based on StackJS https://launchpad.net/stackjs but adopted to be used in Atom Code Editor)
+
---
+++ spec/stack-book-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import StackBookView from '../lib/stack-book-view';
+
+describe('StackBookView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
